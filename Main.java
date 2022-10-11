af657ed00f1f52add4b5059e72bcd870a7306950
dipalidhamal <114014004+dipalidhamal@users.noreply.github.com>
2022-10-11 19:18:57 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,27 +2,27 @@ package com.gl.main;
 
 import java.util.Scanner;
 
-import com.gl.money.Transaction;
+import com.gl.transaction.Transaction;
 
 public class Main {
-
+			
 	public static void main(String[] args) {
 		Transaction transaction = new Transaction();
 		Scanner scanner = new Scanner(System.in);
-		int noOfDenomi;
-		System.out.println("enter the size of currency denominations");
-		noOfDenomi =scanner.nextInt();
-		System.out.println("enter the currency denomination value");
-		int valueOfDenomi;
-		int[] array = new int[noOfDenomi];
-		for (int i = 0; i < noOfDenomi; i++) {
+		int sizeOfArray;
+		System.out.println("enter the size of transaction array");
+		sizeOfArray = scanner.nextInt();
+		System.out.println("enter the value of array");
+		int[] array =new int[sizeOfArray];
+		for (int i = 0; i < sizeOfArray; i++) {
 			array[i]=scanner.nextInt();
 		}
-		System.out.println("enter the amount you want to pay");
-		int amount = scanner.nextInt();
-		transaction.bubbleSort(array);
-		transaction.noOfNotes(array,amount);
-			
+		System.out.println("enter the total number of targets that need to be achieve");
+		int target = scanner.nextInt();
+		for (int i = 0; i < target; i++) {
+			System.out.println("enter the value of target");
+			int value = scanner.nextInt();
+			transaction.isTargetAchievable(value, array);
+		}
 	}
-
-}
+}	
--- Transaction.java
+++ Transaction.java
@@ -1,40 +1,17 @@
-package com.gl.money;
+package com.gl.transaction;
 
 public class Transaction {
-	public void noOfNotes(int[] array, int amount) {
-		int[] result = new int[array.length];
-		for (int i = 0; i < array.length; i++) {
-			if(array[i]<=amount) {
-				result[i] = amount/array[i];
-				amount=amount % array[i];
-				
-			}
-		}if(amount>0) {
-			System.out.println("Amount cannot be made with Denomination ");
-		}else {
-			System.out.println("Your payment approach in order to give min number of notes will be");
-			for (int i = 0; i < result.length; i++) {
-				if(result[i]!=0) {
-					System.out.println(array[i]+":"+result[i]);
-					
-				}
+	public void isTargetAchievable(int target, int[] arr) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum=sum+arr[i];
+			if(sum>=target) {
+				System.out.println("Target achieved after" + (i+1)+"transactions" );
+				break;
 			}
 		}
-		
-	}
-	public void bubbleSort(int[] array) {
-		for (int i=0;i<array.length-1; i++) {
-			for(int j=0;j<array.length-i-1;j++) {
-				if(array[j]<array[j+1]) {
-		
-					//swap
-					int temp = array[j];
-					array[j]= array[j+1];
-					array[j+1]= temp;
-				}
-			}
+		if(sum<target) {
+			System.out.println("Target is not achieved");
 		}
-
 	}
-
 }
